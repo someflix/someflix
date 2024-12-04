347418a08b5ded03775efe6370937d7f78a3a3c6
someflix <github_flix@proton.me>
2024-12-04 20:56:42 +0100

    tabs fix

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -14,6 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
+      "inline-flex h-10 items-center justify-center rounded-full bg-white/20 p-1 text-white backdrop-blur-lg",
       className
     )}
     {...props}
@@ -28,6 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
+      "inline-flex items-center justify-center whitespace-nowrap rounded-full px-3 py-1.5 text-sm font-medium bg-black/40 ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-black/40 data-[state=active]:text-foreground data-[state=active]:shadow-sm",
       className
     )}
     {...props}
